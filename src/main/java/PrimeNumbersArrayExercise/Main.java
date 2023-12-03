0bf5919f4a37e70d1fbe2c1f0fe65b209fd16ee7
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-12-03 19:34:45 -0300

    Added missing Main class on PrimeNumbersArrayExercise

--- src/main/java/PrimeNumbersArrayExercise/Main.java
+++ src/main/java/PrimeNumbersArrayExercise/Main.java
@@ -1,4 +1,15 @@
 package PrimeNumbersArrayExercise;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        PrimeNumbersArray primeNumbersArray = new PrimeNumbersArray();
+        int[] numbersArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int[] resultsArray = primeNumbersArray.filterPrimeNumbers(numbersArray);
+
+        System.out.println("Original numbers array: " + Arrays.toString(numbersArray));
+        System.out.println("Result prime numbers array: " + Arrays.toString(resultsArray));
+    }
 }
